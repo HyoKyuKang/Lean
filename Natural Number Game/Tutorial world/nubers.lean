a476cf56f00a9c3bc3479e5f6fffa86579ec2b06
HyoKyuKang <djs080307@gmail.com>
2025-12-24 17:56:12 +0900

    chore: reorganize project structure

--- Natural Number Game/Tutorial world/nubers.lean
+++
@@ -1,8 +0,0 @@
---In Real Lean4 It's to strong
-example : (2 : Nat) = Nat.succ (Nat.succ 0) := by
-  rfl
-/-
-rw [two_eq_succ_one]
-rw [one_eq_succ_one]
-rfl
--/
--- Natural Number Game/Tutorial world/rw.lean
+++
@@ -1,4 +0,0 @@
---rw : rewrite
-
-example (x y : Nat) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
-  rw [h]
